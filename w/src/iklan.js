ba3a6d73571b1b3f6040d11584f27b5c26f229c6
kash-bots <saul.082908@gmail.com>
2021-03-05 15:38:43 -0300

    First commit

---
+++ w/src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 KASHI〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *PIC PAY,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/15136478596*
+║
+╚═〘 KASHI BOT 〙
+`
+}
+exports.iklan = iklan
---
+++ w/src/vipmenu.js
@@ -0,0 +1,28 @@
+const vipmenu = (prefix) => { 
+	return `            
+	
+╔══✪〘 USUÁRIO VIP 〙✪══
+║
+║lek tu e vip? 😳🤙
+║tmj vey kashi falando
+║
+╠➸ *${prefix}dado*
+╠➸ *${prefix}cekvip*
+╠➸ *${prefix}premiumlist*
+╠➸ *${prefix}delete*
+╠➸ *${prefix}modapk*
+╠➸ *${prefix}indo10*
+╠➸ *${prefix}daftarvip [para virar Premium]*
+╠➸ *${prefix}qrcode*
+╠➥ *${prefix}chentai*
+╠➥ *${prefix}gcpf*
+╠➥ *${prefix}gbin*
+╠➥ *${prefix}pack*
+╠➥ *${prefix}destrava*
+╠➥ *${prefix}gpessoa*
+║
+║
+╚══✪〘  BOT 〙✪══
+`
+}
+exports.vipmenu = vipmenu
